b16b2ad43b5939e3e935ca15ea358e017364fe96
Matthew <msb2170@gmail.com>
2023-11-28 14:25:41 -0500

    prod fix?

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -5,8 +5,8 @@ import type { NextAuthOptions } from "next-auth"
 export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID_DEV as string,
-      clientSecret: process.env.GITHUB_SECRET_DEV as string,
+      clientId: process.env.GITHUB_ID_PROD as string,
+      clientSecret: process.env.GITHUB_SECRET_PROD as string,
     }),
     
   ],
